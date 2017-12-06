e6ab2af04612858c42731d4817258f4169c5ea26
James McClain <jmcclain@azavea.com>
2017-12-06 12:11:45 -0500

    Empty File

---
+++ backend/src/main/scala/Operations.scala
@@ -0,0 +1,14 @@
+package com.azavea.climate
+
+import geotrellis.raster._
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.s3._
+
+object Operations {
+
+  def main(args: Array[String]) : Unit = {
+    println("xxx")
+  }
+
+}

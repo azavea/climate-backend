62f032a93ff2f326810a6a1d4c9338f8c213cc62
James W. McClain <jmcclain@azavea.com>
2018-01-21 17:31:35 -0500

    Remove (Parts Of) Old Indicators

--- backend/src/main/scala/Router.scala
+++ backend/src/main/scala/Router.scala
@@ -150,127 +150,6 @@ object Router {
     }
   }
 
-  def arrayIndicator = {
-    parameter("box" ?, "startTime", "endTime", "divider" ?) { (_box, _startTime, _endTime, _divider) =>
-      val box: Seq[TimedDictionary] => Seq[Double] = _box match {
-        case Some("averageTasmax") => Boxen.averageTasmax
-        case Some("maxTasmin") => Boxen.maxTasmin
-        case _ => Boxen.maxTasmin
-      }
-      val startTime  = ZonedDateTime.parse(_startTime)
-      val endTime = ZonedDateTime.parse(_endTime)
-      val divider: Seq[KV] => Map[ZonedDateTime, Seq[KV]] = _divider match {
-        case Some("month") => Dividers.divideByCalendarMonth
-        case Some("year") => Dividers.divideByCalendarYear
-        case Some("infinity") => Dividers.divideByInfinity
-        case _ => Dividers.divideByCalendarMonth
-      }
-
-      pathEndOrSingleSlash {
-        post {
-          entity(as[String]) { json =>
-            val area: MultiPolygon = json.extractGeometries[MultiPolygon].head
-
-            complete {
-              Operations.futureQuery(
-                startTime, endTime, area,
-                divider,
-                Narrowers.byMean,
-                box
-              ).map({ f =>
-                f.map({ case (_k, _v) =>
-                  val k = _k.format(dateTimeFormat)
-                  val v = _v.toList
-                  k -> v
-                }).toJson
-              })
-            }
-
-          }}}}
-  }
-
-  // ---------------------------------
-
-  def arrayBaselineIndicator = {
-    parameter("box", "startTime", "endTime", "divider" ?, "baseline") { (_box, _startTime, _endTime, _divider, _baseline) =>
-      val baseline = _baseline.toDouble
-      val box: Seq[TimedDictionary] => Seq[Double] = _box match {
-        case "extremePrecipitationEvents" => Boxen.extremePrecipitationEvents(baseline)
-        case _ => throw new Exception
-      }
-      val startTime  = ZonedDateTime.parse(_startTime)
-      val endTime = ZonedDateTime.parse(_endTime)
-      val divider: Seq[KV] => Map[ZonedDateTime, Seq[KV]] = _divider match {
-        case Some("month") => Dividers.divideByCalendarMonth
-        case Some("year") => Dividers.divideByCalendarYear
-        case Some("infinity") => Dividers.divideByInfinity
-        case _ => Dividers.divideByCalendarMonth
-      }
-
-      pathEndOrSingleSlash {
-        post {
-          entity(as[String]) { json =>
-            val area: MultiPolygon = json.extractGeometries[MultiPolygon].head
-
-            complete {
-              Operations.futureQuery(
-                startTime, endTime, area,
-                divider,
-                Narrowers.byMean,
-                box
-              ).map({ f =>
-                f.map({ case (_k, _v) =>
-                  val k = _k.format(dateTimeFormat)
-                  val v = _v.toList
-                  k -> v
-                }).toJson
-              })
-            }
-
-          }}}}
-  }
-
-  // ---------------------------------
-
-  def arrayPredicateIndicator = {
-    parameter("box", "startTime", "endTime", "divider" ?, "baseline") { (_box, _startTime, _endTime, _divider, _baseline) =>
-      val baseline = _baseline.toDouble
-      val box: Seq[TimedDictionary] => Seq[Double] = _box match {
-        case "heatWaveDurationIndex" => Boxen.heatWaveDurationIndex(baseline)
-        case _ => throw new Exception
-      }
-      val startTime  = ZonedDateTime.parse(_startTime)
-      val endTime = ZonedDateTime.parse(_endTime)
-      val divider: Seq[KV] => Map[ZonedDateTime, Seq[KV]] = _divider match {
-        case Some("month") => Dividers.divideByCalendarMonth
-        case Some("year") => Dividers.divideByCalendarYear
-        case Some("infinity") => Dividers.divideByInfinity
-        case _ => Dividers.divideByCalendarMonth
-      }
-
-      pathEndOrSingleSlash {
-        post {
-          entity(as[String]) { json =>
-            val area: MultiPolygon = json.extractGeometries[MultiPolygon].head
-
-            complete {
-              Operations.futureQuery(
-                startTime, endTime, area,
-                divider,
-                Narrowers.byMean,
-                box
-              ).map({ f =>
-                f.map({ case (_k, _v) =>
-                  val k = _k.format(dateTimeFormat)
-                  val v = _v.toList
-                  k -> v
-                }).toJson
-              })
-            }
-
-          }}}}
-  }
-
   // ---------------------------------
   val rng = scala.util.Random
 
@@ -361,9 +240,6 @@ object Router {
   def routes() =
     cors(settings) {
       pathPrefix("indicator") { indicator } ~
-      pathPrefix("arrayIndicator") { arrayIndicator } ~
-      pathPrefix("arrayBaselineIndicator") { arrayBaselineIndicator } ~
-      pathPrefix("arrayPredicateIndicator") { arrayPredicateIndicator } ~
       pathPrefix("s3month") { s3month } ~ pathPrefix("s3year") { s3month } ~ pathPrefix("s3decade") { s3decade }
     }
 
